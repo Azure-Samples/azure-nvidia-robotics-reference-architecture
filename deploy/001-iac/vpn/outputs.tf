6d1c7ce8e59dd6a5a0289e6627e6f89137ca9a83
Allen Greaves <allengreaves@microsoft.com>
2025-11-28 18:31:04 -0800

    feat(vpn): add VPN Gateway module with P2S and S2S connectivity
    Create reusable VPN module and standalone configuration for Azure VPN Gateway
    deployment supporting Point-to-Site and Site-to-Site VPN connections.
    
    - Add modules/vpn/ with GatewaySubnet, Public IP, VPN Gateway resources
    - Add vpn/ standalone configuration following dns/ folder pattern
    - Support certificate-based P2S authentication
    - Support multiple S2S connections with IPsec policies
    - Use data sources to reference existing VNet infrastructure

---
+++ deploy/001-iac/vpn/outputs.tf
@@ -0,0 +1,41 @@
+/*
+ * VPN Gateway Outputs
+ */
+
+output "vpn_gateway" {
+  description = "VPN Gateway resource details"
+  value       = module.vpn.vpn_gateway
+}
+
+output "vpn_gateway_public_ip" {
+  description = "Public IP address of the VPN Gateway"
+  value       = module.vpn.vpn_gateway_public_ip
+}
+
+output "gateway_subnet" {
+  description = "Gateway subnet details"
+  value       = module.vpn.gateway_subnet
+}
+
+/*
+ * P2S Connection Info
+ */
+
+output "p2s_connection_info" {
+  description = "Point-to-Site VPN connection information"
+  value       = module.vpn.p2s_connection_info
+}
+
+/*
+ * S2S Connection Outputs
+ */
+
+output "site_connections" {
+  description = "Site-to-Site VPN connection details"
+  value       = module.vpn.site_connections
+}
+
+output "local_network_gateways" {
+  description = "Local network gateway details for each site"
+  value       = module.vpn.local_network_gateways
+}
